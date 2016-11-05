27ede96ae8c8ff3df83c626cfd1418d773d0cc4e
Michael Möhrle <mi.moehrle@gmail.com>
2016-11-05 18:50:01 +0100

    Move definitions into correct file

---
+++ src/detail/task_pool.cpp
@@ -0,0 +1,22 @@
+#include "../../include/detail/task_pool.h"
+
+namespace gothreads {
+    namespace detail {
+        bool task_pool::empty() const {
+            return _map.empty();
+        }
+
+        task& task_pool::next() {
+            class notimplemented {};
+            throw notimplemented();
+            return task();
+        }
+
+        task const& task_pool::next() const {
+            class notimplemented {};
+            throw notimplemented();
+            return task();
+        }
+
+    }
+}
--- src/detail/thread_pool.cpp
+++ src/detail/thread_pool.cpp
@@ -1,22 +1,7 @@
 #include "../../include/detail/thread_pool.h"
-#include "../../include/detail/task_pool.h"
 
 namespace gothreads {
     namespace detail {
-        bool task_pool::empty() const {
-            return _map.empty();
-        }
-
-        task& task_pool::next() {
-            class notimplemented {};
-            throw notimplemented();
-            return task();
-        }
-
-        task const& task_pool::next() const {
-            class notimplemented {};
-            throw notimplemented();
-            return task();
         }
     }
 }
